2bbad4b813d917b0b52fb4c81eac9bbe9c51de3c
deleonjeremy <moragajeremy@gmail.com>
2019-10-28 22:16:18 -0500

    Added a player model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_025626) do
+ActiveRecord::Schema.define(version: 2019_10_29_031025) do
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.integer "team_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["team_id"], name: "index_players_on_team_id"
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
@@ -18,4 +26,5 @@ ActiveRecord::Schema.define(version: 2019_10_29_025626) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "players", "teams"
 end
